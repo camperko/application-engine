3439cc1970fa218e96c1c3df1f164b822671fc21
Jozef Daxner <daxner.dodo@gmail.com>
2023-01-15 23:49:48 +0100

    [dp_daxner]
    - add communication with backends

---
+++ src/main/java/com/netgrif/application/engine/workflow/service/PreprocessorRegistrationService.java
@@ -0,0 +1,69 @@
+package com.netgrif.application.engine.workflow.service;
+
+import com.netgrif.application.engine.importer.model.Document;
+import com.netgrif.application.engine.workflow.web.requestbodies.MultipartFileResource;
+import com.netgrif.application.engine.workflow.web.responsebodies.RegistrationDataResponse;
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Service
+public class PreprocessorRegistrationService {
+
+    private RestTemplate restTemplate;
+
+    @Value("${nae.registration-service.address}")
+    private String REGISTRATION_SERVICE_ADDRESS;
+
+    private static final String PREPROCESSING_FILE_NAME = "preprocessing_file";
+
+    public PreprocessorRegistrationService() {
+        this.restTemplate = new RestTemplate();
+    }
+
+    public Document doPreprocessing(Document document, InputStream xml) throws IOException {
+        if (document.getPreprocessor() != null && !document.getPreprocessor().equals("")) {
+            String url = REGISTRATION_SERVICE_ADDRESS + document.getPreprocessor();
+            ResponseEntity<RegistrationDataResponse> responseData = restTemplate.exchange(
+                    url, HttpMethod.GET, new HttpEntity<>(new HttpHeaders()), RegistrationDataResponse.class
+            );
+
+            if (responseData.getBody() == null || responseData.getBody().getStringId() == null) {
+                return document;
+            }
+
+            byte[] content = IOUtils.toByteArray(xml);
+            Document doc = new Document();
+
+            HttpHeaders parts = new HttpHeaders();
+            parts.setContentType(MediaType.APPLICATION_XML);
+            final HttpEntity<MultipartFileResource> partsEntity = new HttpEntity<>(new MultipartFileResource(content, PREPROCESSING_FILE_NAME), parts);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+            MultiValueMap<String, Object> requestMap = new LinkedMultiValueMap<>();
+            requestMap.add("file", partsEntity);
+
+            ResponseEntity<String> response = restTemplate.exchange(
+                    responseData.getBody().getAddress(), HttpMethod.POST, new HttpEntity<>(requestMap, headers), String.class
+            );
+
+//            MultiValueMap<String, Object> requestWrapper = new LinkedMultiValueMap<>();
+//            requestWrapper.add("file", new MultipartFileResource(content, PREPROCESSING_FILE_NAME));
+//            HttpEntity<MultiValueMap<String, Object>> entity = new HttpEntity<>(requestWrapper, headers);
+//            ObjectNode response = this.restTemplate.postForObject(responseData.getBody().getAddress(), requestWrapper, ObjectNode.class);
+//            ResponseEntity<ObjectNode> response = restTemplate.exchange(responseData.getBody().getAddress(), HttpMethod.POST, entity, ObjectNode.class);
+
+            // TODO: parse document from response (change response to xml file)
+            return doc;
+        }
+        return document;
+    }
+}
---
+++ src/main/java/com/netgrif/application/engine/workflow/web/responsebodies/RegistrationDataResponse.java
@@ -0,0 +1,25 @@
+package com.netgrif.application.engine.workflow.web.responsebodies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegistrationDataResponse {
+
+    private String stringId;
+
+    private String name;
+
+    private String address;
+
+    private String description;
+
+    private LocalDateTime creationDate;
+
+    private LocalDateTime modificationDate;
+}
